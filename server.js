8cc34dc249955188dda551b24c603d7bd69c5478
Agnes Ching <agnesching19@gmail.com>
2018-03-10 09:28:04 +0000

    rendering templates with data

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = require('hbs');
+
 const app = express();
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (request, response) => {
-  // response.send('<h1>Hello Express!</h1>');
-  response.send({
-    name: 'Agnes',
-    sports: [
-      'running',
-      'swimming'
-    ]
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
