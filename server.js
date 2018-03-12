33e06cd8b5dd8439f573b785bf247d5147165571
Agnes Ching <agnesching19@gmail.com>
2018-03-12 21:03:35 +0000

    advanced templating

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
 const hbs = require('hbs');
 
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
-app.set('views', '.views/layouts/');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website',
-    currentYear: new Date().getFullYear()
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    message: 'Some text gere',
-    currentYear: new Date().getFullYear()
+    pageTitle: 'About Page'
   });
 });
 
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
-    errorMessage: '404 - Wrong turn'
+    errorMessage: 'Unable to handle request'
   });
 });
 
 app.listen(3000, () => {
-  console.log('Server is up on port 3000.')
+  console.log('Server is up on port 3000');
 });
